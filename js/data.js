8af37668e6864d7ab8fee8e5e3b4a0284d3d0a8c
ratoobi <69602231+ratoobi@users.noreply.github.com>
2021-10-20 23:41:27 -0300

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,372 @@
+const data = [
+  {
+    name: {
+      first: "Grana",
+      last: "Padano",
+    },
+    type: "Hard Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/grana-padano.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Parmigiano",
+      last: "Reggiano",
+    },
+    type: "Hard Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/parmigiano-reggiano.jpg",
+    },
+  },
+  
+  {
+    name: {
+      first: "Cheddar",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "England",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/cheddar.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Comté",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "France",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/comte.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Edam",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "Netherlands",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/edam.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Emmentaler",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "Switzerland",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/emmentaler.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Fontina",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/fontina.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Gruyère",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "Switzerland",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/gruyere.png",
+    },
+  },
+  {
+    name: {
+      first: "Gouda",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "Netherlands",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/gouda.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Havarti",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "Denmark",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/havarti.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Provolone",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/provolone.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Taleggio",
+      last: "",
+    },
+    type: "Semi Hard Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/taleggio.jpg",
+    },
+  },
+
+  {
+    name: {
+      first: "Castello",
+      last: "Creamy-Blue",
+    },
+    type: "Blue Mould Cheese",
+    origin: {
+      country: "Denmark",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/castello-creamy-blue.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Danish",
+      last: "Blue",
+    },
+    type: "Blue Mould Cheese",
+    origin: {
+      country: "Denmark",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/danish-blue.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Gorgonzola",
+      last: "Blue",
+    },
+    type: "Blue Mould Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/gorgonzola.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Roquefort",
+      last: "Blue",
+    },
+    type: "Blue Mould Cheese",
+    origin: {
+      country: "France",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/roquefort.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Stilton",
+      last: "Blue",
+    },
+    type: "Blue Mould Cheese",
+    origin: {
+      country: "England",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/stilton.jpg",
+    },
+  },
+
+  {
+    name: {
+      first: "Pecorino",
+      last: "",
+    },
+    type: "Hard Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/pecorino.jpg",
+    },
+  },
+
+  {
+    name: {
+      first: "Brie",
+      last: "",
+    },
+    type: "White Mould Cheese",
+    origin: {
+      country: "France",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/brie.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Camembert",
+      last: "",
+    },
+    type: "White Mould Cheese",
+    origin: {
+      country: "France",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/camembert.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Creamy",
+      last: "Brie",
+    },
+    type: "White Mould Cheese",
+    origin: {
+      country: "France",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/creamy-brie.jpg",
+    },
+  },
+
+  {
+    name: {
+      first: "Burrata",
+      last: "",
+    },
+    type: "Fresh Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/burrata.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Cream",
+      last: "Cheese",
+    },
+    type: "Fresh Cheese",
+    origin: {
+      country: "United States of America",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/cream-cheese.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Feta",
+      last: "",
+    },
+    type: "Fresh Cheese",
+    origin: {
+      country: "Greece",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/feta.jpg",
+    },
+  },
+  {
+    name: {
+      first: "Mozzarella",
+      last: "",
+    },
+    type: "Fresh Cheese",
+    origin: {
+      country: "Italy",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/mozzarella.png",
+    },
+  },
+
+  {
+    name: {
+      first: "Chèvre",
+      last: "",
+    },
+    type: "Goat Cheese",
+    origin: {
+      country: "France",
+      date: "12-15-2005",
+    },
+    picture: {
+      large: "./img/cheese/chevre.jpg",
+    },
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,100 @@
+const linkList = document.querySelector("ul.link-list");
+const header = document.querySelector("header");
+let dataCopy = data;
+let lastPage = 0;
+header.innerHTML = "";
+header.innerHTML += `
+<h2>Cheeses</h2>
+<label for="search" class="cheese-search">
+<span>Search by name</span>
+<input id="search" placeholder="Search by name...">
+<button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+</label>
+`;
+
+showPage = (list, page) => {
+  const cheeseList = document.querySelector("ul.cheese-list");
+  let startIndex = page * 9;
+  let endIndex = page * 9 + 8;
+  if (endIndex >= list.length) endIndex = list.length - 1;
+  if (list.length < 9) {
+    startIndex = 0;
+    endIndex = list.length - 1;
+  }
+  if (page === 0 && list.length >= 9) {
+    startIndex = 0;
+    endIndex = 8;
+  }
+  cheeseList.innerHTML = "";
+
+  for (let index = startIndex; index <= endIndex; index++) {
+    cheeseList.innerHTML += `
+      <li class="cheese-item">
+      <div class="cheese-details">
+      <img class="avatar" src="${list[index].picture.large}" alt="Cheese Picture">
+        <h3>${list[index].name.first} ${list[index].name.last}</h3>
+        <span class="type">${list[index].type}</span>
+      </div>
+      <div class="origin-details">
+        <span class="origin">Origin: ${list[index].origin.country}</span>
+      </div>
+    </li>
+    `;  
+  }
+  if (list.length === 0) {
+    cheeseList.innerHTML = '<h3 class="warning-message">No results found</h3>';
+  }
+};
+
+addPagination = (list) => {
+  const numberOfPages = Math.ceil(list.length / 9);
+  linkList.innerHTML = "";
+
+  for (let index = 1; index <= numberOfPages; index++) {
+    linkList.insertAdjacentHTML(
+      "beforeend",
+      `<li><button type="button">${index}</button></li>`
+    );
+    linkList.getElementsByTagName("button")[0].className = "active";
+  }
+};
+
+searchCheese = (list, name) => {
+  let regex = new RegExp(name, "i");
+  let newList = [];
+
+  for (let index = 0; index < list.length; index++) {
+    let fullName = list[index].name.first + " " + list[index].name.last;
+    if (
+      regex.test(list[index].name.first) ||
+      regex.test(list[index].name.last) ||
+      regex.test(fullName)
+    ) {
+      newList.push(list[index]);
+    }
+  }
+  dataCopy = newList;
+  showPage(newList, 0);
+  addPagination(newList);
+};
+
+linkList.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    linkList.getElementsByTagName("button")[lastPage].className = "";
+    e.target.className = "active";
+    lastPage = parseInt(e.target.textContent) - 1;
+    showPage(dataCopy, lastPage);
+  }
+});
+
+showPage(data, 0);
+addPagination(data);
+
+const searchBar = document.querySelector("#search");
+const searchButton = document.querySelector("label button");
+searchBar.addEventListener("keyup", () => {
+  searchCheese(data, searchBar.value);
+});
+searchButton.addEventListener("click", () => {
+  searchCheese(data, searchBar.value);
+});
